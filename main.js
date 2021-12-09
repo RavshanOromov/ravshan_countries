a575b77f54915083c7fc163ac896677a316c90d7
--replace <72220075+RavshanOromov@users.noreply.github.com>
2021-12-09 16:17:42 +0900

    commit2

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const elDarkMode = document.querySelector('.dark-mode-link');
+const elDarkMode = document.querySelector('.dark-mode-button');
 
 elDarkMode.addEventListener('click', function(){
 document.body.classList.add('dark-mode');
+});
+elDarkMode.addEventListener('doubleclick', function(){
+document.body.classList.remove('dark-mode');
 });
