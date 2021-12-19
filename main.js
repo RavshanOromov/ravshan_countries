d7bcd4e158e644490e52f7df62234624485796bc
--replace <72220075+RavshanOromov@users.noreply.github.com>
2021-12-19 11:13:57 +0900

    js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 const elDarkMode = document.querySelector('.dark-mode-button');
 
 elDarkMode.addEventListener('click', function(){
-document.body.classList.toggle('dark-mode');
-<<<<<<< HEAD
-});
-=======
-});
->>>>>>> 885ba4691fdc66f58ff6d047bb0eb0308fecebcc
+  document.body.classList.toggle('dark-mode') });
